73704727cacfc16c60df4e1933a6042da8ba1dbf
Erekle2520 <132439726+Erekle2520@users.noreply.github.com>
2023-05-08 15:45:01 +0400

    Create hello3.py

---
+++ hello3.py
@@ -0,0 +1,7 @@
+name=input("What is your name? ")
+if name=="Erekle":
+    print("Hello " + name+".")
+elif name=="Giorgi":
+    print("\"Good\" to see you,",name)
+else:
+    print(f"Nice to meet you {name}")
