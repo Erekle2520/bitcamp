f8b3c8d3fc4eb21fca0e49f08926858e4d0d9f88
Erekle2520 <132439726+Erekle2520@users.noreply.github.com>
2023-05-08 15:43:57 +0400

    Create playback.py

---
+++ playback.py
@@ -0,0 +1,14 @@
+#text = input("Tell me something and i will rewrite it for you correctly: ")
+#replace = text.replace(' ','...')
+
+
+#print(replace)
+
+def main():
+    text=input("Write something and I will slow it down: ")
+    #slowdown=text.replace(" ","...")
+    print(slowdown(text))
+
+def slowdown(text)
+    return text=text.replace(" ","...")
+main()
