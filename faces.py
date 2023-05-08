312da0af5f179c858488a9949cc5695e910ed36b
Erekle2520 <132439726+Erekle2520@users.noreply.github.com>
2023-05-08 15:45:59 +0400

    Create faces.py

---
+++ faces.py
@@ -0,0 +1,12 @@
+def main():
+    greating=input("Hello or Goodbye: ")
+    if greating=="Hello :)":
+        print("Hello 🙂")
+    elif greating=="Goodbye :(":
+        print("Goodbye 🙁")
+    elif greating=="Hello :) Goodbye :(":
+        print("Hello 🙂 Goodbye 🙁")
+    else:
+        print("try onemore")
+
+main()
