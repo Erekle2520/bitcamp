2e04a5ff48b79ed88e35a5ac686ce8fa18b8ca81
Erekle2520 <132439726+Erekle2520@users.noreply.github.com>
2023-05-10 23:58:57 +0400

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,13 @@
+def main():
+# Ask user for greeting and clean whitespaces in output
+
+Greeting=input("Greeting: ").strip()
+
+if Greeting.startswith("Hello"):
+    print("$0")
+elif Greeting.startswith("H"):
+    print("$20")
+else:
+    print("$100")
+
+main()
